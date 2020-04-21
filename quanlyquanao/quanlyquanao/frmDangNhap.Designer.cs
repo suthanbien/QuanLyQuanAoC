42db02d8c0c3bf305053a1b8575c9e04ce8093e5
lecongtuan8899 <55322707+lecongtuan8899@users.noreply.github.com>
2020-04-21 15:30:20 +0700

    quanlyquanao
    teamwork

---
+++ quanlyquanao/quanlyquanao/frmDangNhap.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace quanlyquanao
+{
+    partial class frmdangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmdangnhap));
+            this.lbltentaikhoan = new System.Windows.Forms.Label();
+            this.lblmatkhau = new System.Windows.Forms.Label();
+            this.txttentaikhoan = new System.Windows.Forms.TextBox();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.rbquanly = new System.Windows.Forms.RadioButton();
+            this.rbnhanvien = new System.Windows.Forms.RadioButton();
+            this.btndangnhap = new System.Windows.Forms.Button();
+            this.alblquenmatkhau = new System.Windows.Forms.LinkLabel();
+            this.albldoimatkhau = new System.Windows.Forms.LinkLabel();
+            this.grbtucach = new System.Windows.Forms.GroupBox();
+            this.pnldangnhap = new System.Windows.Forms.Panel();
+            this.lbldangnhap = new System.Windows.Forms.Label();
+            this.grbtucach.SuspendLayout();
+            this.pnldangnhap.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbltentaikhoan
+            // 
+            this.lbltentaikhoan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbltentaikhoan.AutoSize = true;
+            this.lbltentaikhoan.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbltentaikhoan.Location = new System.Drawing.Point(5, 73);
+            this.lbltentaikhoan.Name = "lbltentaikhoan";
+            this.lbltentaikhoan.Size = new System.Drawing.Size(122, 22);
+            this.lbltentaikhoan.TabIndex = 1;
+            this.lbltentaikhoan.Text = "Tên tài khoản:";
+            // 
+            // lblmatkhau
+            // 
+            this.lblmatkhau.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblmatkhau.AutoSize = true;
+            this.lblmatkhau.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblmatkhau.Location = new System.Drawing.Point(5, 113);
+            this.lblmatkhau.Name = "lblmatkhau";
+            this.lblmatkhau.Size = new System.Drawing.Size(88, 22);
+            this.lblmatkhau.TabIndex = 2;
+            this.lblmatkhau.Text = "Mật khẩu:";
+            // 
+            // txttentaikhoan
+            // 
+            this.txttentaikhoan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txttentaikhoan.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txttentaikhoan.Location = new System.Drawing.Point(142, 65);
+            this.txttentaikhoan.Name = "txttentaikhoan";
+            this.txttentaikhoan.Size = new System.Drawing.Size(208, 30);
+            this.txttentaikhoan.TabIndex = 3;
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtmatkhau.Location = new System.Drawing.Point(142, 105);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(208, 30);
+            this.txtmatkhau.TabIndex = 4;
+            // 
+            // rbquanly
+            // 
+            this.rbquanly.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rbquanly.AutoSize = true;
+            this.rbquanly.Checked = true;
+            this.rbquanly.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.rbquanly.Location = new System.Drawing.Point(43, 36);
+            this.rbquanly.Name = "rbquanly";
+            this.rbquanly.Size = new System.Drawing.Size(105, 22);
+            this.rbquanly.TabIndex = 6;
+            this.rbquanly.TabStop = true;
+            this.rbquanly.Text = "Quản Lý";
+            this.rbquanly.UseVisualStyleBackColor = true;
+            // 
+            // rbnhanvien
+            // 
+            this.rbnhanvien.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rbnhanvien.AutoSize = true;
+            this.rbnhanvien.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.rbnhanvien.Location = new System.Drawing.Point(184, 36);
+            this.rbnhanvien.Name = "rbnhanvien";
+            this.rbnhanvien.Size = new System.Drawing.Size(122, 22);
+            this.rbnhanvien.TabIndex = 7;
+            this.rbnhanvien.Text = "Nhân Viên";
+            this.rbnhanvien.UseVisualStyleBackColor = true;
+            // 
+            // btndangnhap
+            // 
+            this.btndangnhap.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btndangnhap.BackColor = System.Drawing.Color.Tomato;
+            this.btndangnhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btndangnhap.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btndangnhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndangnhap.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btndangnhap.ForeColor = System.Drawing.Color.White;
+            this.btndangnhap.Location = new System.Drawing.Point(9, 264);
+            this.btndangnhap.Name = "btndangnhap";
+            this.btndangnhap.Size = new System.Drawing.Size(341, 50);
+            this.btndangnhap.TabIndex = 10;
+            this.btndangnhap.Text = "Đăng Nhập";
+            this.btndangnhap.UseVisualStyleBackColor = false;
+            // 
+            // alblquenmatkhau
+            // 
+            this.alblquenmatkhau.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.alblquenmatkhau.AutoSize = true;
+            this.alblquenmatkhau.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.alblquenmatkhau.Font = new System.Drawing.Font("Times New Roman", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.alblquenmatkhau.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.alblquenmatkhau.Location = new System.Drawing.Point(110, 400);
+            this.alblquenmatkhau.Name = "alblquenmatkhau";
+            this.alblquenmatkhau.Size = new System.Drawing.Size(149, 23);
+            this.alblquenmatkhau.TabIndex = 11;
+            this.alblquenmatkhau.TabStop = true;
+            this.alblquenmatkhau.Text = "Quên mật khẩu?";
+            // 
+            // albldoimatkhau
+            // 
+            this.albldoimatkhau.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.albldoimatkhau.AutoSize = true;
+            this.albldoimatkhau.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.albldoimatkhau.Font = new System.Drawing.Font("Times New Roman", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.albldoimatkhau.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.albldoimatkhau.Location = new System.Drawing.Point(120, 434);
+            this.albldoimatkhau.Name = "albldoimatkhau";
+            this.albldoimatkhau.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.albldoimatkhau.Size = new System.Drawing.Size(124, 23);
+            this.albldoimatkhau.TabIndex = 12;
+            this.albldoimatkhau.TabStop = true;
+            this.albldoimatkhau.Text = "Đổi mật khẩu";
+            // 
+            // grbtucach
+            // 
+            this.grbtucach.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.grbtucach.Controls.Add(this.rbquanly);
+            this.grbtucach.Controls.Add(this.rbnhanvien);
+            this.grbtucach.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.grbtucach.ForeColor = System.Drawing.Color.Black;
+            this.grbtucach.Location = new System.Drawing.Point(9, 156);
+            this.grbtucach.Name = "grbtucach";
+            this.grbtucach.Size = new System.Drawing.Size(341, 87);
+            this.grbtucach.TabIndex = 14;
+            this.grbtucach.TabStop = false;
+            this.grbtucach.Text = "Tư cách";
+            // 
+            // pnldangnhap
+            // 
+            this.pnldangnhap.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnldangnhap.BackColor = System.Drawing.Color.White;
+            this.pnldangnhap.Controls.Add(this.lbldangnhap);
+            this.pnldangnhap.Controls.Add(this.albldoimatkhau);
+            this.pnldangnhap.Controls.Add(this.grbtucach);
+            this.pnldangnhap.Controls.Add(this.alblquenmatkhau);
+            this.pnldangnhap.Controls.Add(this.lbltentaikhoan);
+            this.pnldangnhap.Controls.Add(this.btndangnhap);
+            this.pnldangnhap.Controls.Add(this.lblmatkhau);
+            this.pnldangnhap.Controls.Add(this.txtmatkhau);
+            this.pnldangnhap.Controls.Add(this.txttentaikhoan);
+            this.pnldangnhap.Location = new System.Drawing.Point(238, 53);
+            this.pnldangnhap.Name = "pnldangnhap";
+            this.pnldangnhap.Size = new System.Drawing.Size(360, 467);
+            this.pnldangnhap.TabIndex = 16;
+            // 
+            // lbldangnhap
+            // 
+            this.lbldangnhap.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbldangnhap.AutoSize = true;
+            this.lbldangnhap.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.lbldangnhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbldangnhap.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbldangnhap.ForeColor = System.Drawing.Color.White;
+            this.lbldangnhap.Location = new System.Drawing.Point(0, 0);
+            this.lbldangnhap.MinimumSize = new System.Drawing.Size(360, 50);
+            this.lbldangnhap.Name = "lbldangnhap";
+            this.lbldangnhap.Size = new System.Drawing.Size(360, 50);
+            this.lbldangnhap.TabIndex = 15;
+            this.lbldangnhap.Text = "ĐĂNG NHẬP";
+            this.lbldangnhap.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmdangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = global::quanlyquanao.Properties.Resources.sounds_aura_hp2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(824, 585);
+            this.Controls.Add(this.pnldangnhap);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmdangnhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quần Áo Sỉ Lẻ Baby Milo";
+            this.grbtucach.ResumeLayout(false);
+            this.grbtucach.PerformLayout();
+            this.pnldangnhap.ResumeLayout(false);
+            this.pnldangnhap.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbltentaikhoan;
+        private System.Windows.Forms.Label lblmatkhau;
+        private System.Windows.Forms.TextBox txttentaikhoan;
+        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.RadioButton rbquanly;
+        private System.Windows.Forms.RadioButton rbnhanvien;
+        private System.Windows.Forms.Button btndangnhap;
+        private System.Windows.Forms.LinkLabel alblquenmatkhau;
+        private System.Windows.Forms.LinkLabel albldoimatkhau;
+        private System.Windows.Forms.GroupBox grbtucach;
+        private System.Windows.Forms.Panel pnldangnhap;
+        private System.Windows.Forms.Label lbldangnhap;
+    }
+}
+
