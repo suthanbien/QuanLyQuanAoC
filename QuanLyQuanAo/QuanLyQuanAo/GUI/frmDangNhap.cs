70306f8913c8ef133d5a13feb863b546b7df140a
lecongtuan8899 <55322707+lecongtuan8899@users.noreply.github.com>
2020-04-22 19:29:55 +0700

    Revert "XoaHetLAmLai"
    This reverts commit 308660a0bdd72db34176965306b32309627f2bb3.

---
+++ QuanLyQuanAo/QuanLyQuanAo/GUI/frmDangNhap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quanlyquanao
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void lblDangNhap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void llblQuenMatKhau_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmQuenMatKhau frm = new frmQuenMatKhau();
+            frm.ShowDialog();
+        }
+    }
+}
