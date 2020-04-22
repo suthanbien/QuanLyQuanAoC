c67f415e37664e8fdcbf056cc5f2c82c8d0f90d9
Quy <suthanbien@gmail.com>
2020-04-22 22:16:11 +0700

    uplai

---
+++ QuanLyQuanAo/QuanLyQuanAo/GUI/frmQuenMatKhau.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace quanlyquanao
+{
+    partial class frmQuenMatKhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmQuenMatKhau));
+            this.lblThongBao = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnXacNhan = new System.Windows.Forms.Button();
+            this.ttQuenMatKhau = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lblThongBao
+            // 
+            this.lblThongBao.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblThongBao.AutoSize = true;
+            this.lblThongBao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblThongBao.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblThongBao.ForeColor = System.Drawing.Color.Black;
+            this.lblThongBao.Location = new System.Drawing.Point(41, 37);
+            this.lblThongBao.MaximumSize = new System.Drawing.Size(50, 50);
+            this.lblThongBao.MinimumSize = new System.Drawing.Size(500, 30);
+            this.lblThongBao.Name = "lblThongBao";
+            this.lblThongBao.Size = new System.Drawing.Size(500, 44);
+            this.lblThongBao.TabIndex = 0;
+            this.lblThongBao.Text = "Hãy nhập email đã đăng ký, một mã 6 số sẽ được gửi đến email của bạn, xác nhận để" +
+    " lấy lại mật khẩu.";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblEmail.ForeColor = System.Drawing.Color.Black;
+            this.lblEmail.Location = new System.Drawing.Point(41, 141);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(63, 22);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtEmail.BackColor = System.Drawing.Color.White;
+            this.txtEmail.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtEmail.ForeColor = System.Drawing.Color.Black;
+            this.txtEmail.Location = new System.Drawing.Point(110, 133);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(415, 30);
+            this.txtEmail.TabIndex = 2;
+            this.ttQuenMatKhau.SetToolTip(this.txtEmail, "Nhập Email Đã Đăng Ký Từ Trước");
+            // 
+            // btnXacNhan
+            // 
+            this.btnXacNhan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnXacNhan.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnXacNhan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnXacNhan.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnXacNhan.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnXacNhan.ForeColor = System.Drawing.Color.White;
+            this.btnXacNhan.Location = new System.Drawing.Point(369, 193);
+            this.btnXacNhan.Name = "btnXacNhan";
+            this.btnXacNhan.Size = new System.Drawing.Size(156, 45);
+            this.btnXacNhan.TabIndex = 3;
+            this.btnXacNhan.Text = "Xác Nhận";
+            this.ttQuenMatKhau.SetToolTip(this.btnXacNhan, "Xác Nhận ");
+            this.btnXacNhan.UseVisualStyleBackColor = false;
+            // 
+            // ttQuenMatKhau
+            // 
+            this.ttQuenMatKhau.ForeColor = System.Drawing.Color.Black;
+            // 
+            // frmQuenMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(591, 271);
+            this.Controls.Add(this.btnXacNhan);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblThongBao);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmQuenMatKhau";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lấy Lại Mật Khẩu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblThongBao;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnXacNhan;
+        private System.Windows.Forms.ToolTip ttQuenMatKhau;
+    }
+}
