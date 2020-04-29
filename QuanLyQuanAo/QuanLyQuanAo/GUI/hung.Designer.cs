23377918ab47c9cead410dc034c94a8f1740c4ed
minhhung130497 <55131448+minhhung130497@users.noreply.github.com>
2020-04-29 14:18:54 +0700

    hunnng

--- QuanLyQuanAo/QuanLyQuanAo/GUI/hung.Designer.cs
+++ QuanLyQuanAo/QuanLyQuanAo/GUI/hung.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // hung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(639, 351);
+            this.Name = "hung";
             this.Text = "hung";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
