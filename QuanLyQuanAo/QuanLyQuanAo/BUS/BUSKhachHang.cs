ab974a3691540e3dae5d01103e448653be3fca00
Quy <suthanbien@gmail.com>
2020-06-25 14:51:10 +0700

    Quy: UpLai

--- QuanLyQuanAo/QuanLyQuanAo/BUS/BUSKhachHang.cs
+++ QuanLyQuanAo/QuanLyQuanAo/BUS/BUSKhachHang.cs
@@ -1,6 +1,7 @@
 ﻿using QuanLyQuanAo.DAO;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace QuanLyQuanAo.BUS
         #region các khai báo
         private static BUSKhachHang instance;
         List<ClassKhachHang> lstKhachHang = new List<ClassKhachHang>();
-        List<string> lstChucVu = new List<string>();
+        List<string> lstLoaiKH = new List<string>();
         #endregion
 
         #region các phương thức
@@ -38,11 +39,110 @@ namespace QuanLyQuanAo.BUS
         {
             lstKhachHang = DAOKhachHang.Instance.GetKhachHang();
             dgv.DataSource = lstKhachHang;
-           // GetListChucVu();
+            GetListLoaiKH();
+        }
+        public List<string> GetListLoaiKH()
+        {
+            lstLoaiKH = DAOKhachHang.Instance.GetListLoaiKH();
+            return lstLoaiKH;
         }
         #endregion
 
         #region các xử lý
+       
+        public void AddRows(DataGridView dgvHangHoa, DataGridViewComboBoxCell comboBoxCell)
+        {
+            dgvHangHoa.DataSource = null;
+            ClassKhachHang NVNew2 = new ClassKhachHang();
+            lstKhachHang.Add(NVNew2);
+            dgvHangHoa.DataSource = lstKhachHang;
+
+            comboBoxCell.DataSource = lstLoaiKH;
+
+        }
+
+
+        public void LoadComboBoxLoaiKH(DataGridViewComboBoxCell comboBoxCellHSX)
+        {
+            comboBoxCellHSX.DataSource = lstLoaiKH;
+        }
+
+        public void DelRow(DataGridView dgvNhanVien)
+        {
+            lstKhachHang.RemoveAt(lstKhachHang.Count - 1);
+            dgvNhanVien.DataSource = null;
+            dgvNhanVien.DataSource = lstKhachHang;
+        }
+
+
+
+        public bool Them(int maKH,  string tenKH,DateTime ngaySinhKH,string gioiTinh,
+                     string   email,string diaChi,string soDT,string loaiKH,string chuThich)
+        {
+            return DAOKhachHang.Instance.Them(maKH, tenKH, ngaySinhKH, gioiTinh,
+                        email, diaChi, soDT, loaiKH, chuThich);
+        }
+
+        public bool Sua(int maKH, string tenKH, DateTime ngaySinhKH, string gioiTinh,
+                     string email, string diaChi, string soDT, string loaiKH, string chuThich)
+        {
+            return DAOKhachHang.Instance.Sua(maKH, tenKH, ngaySinhKH, gioiTinh,
+                        email, diaChi, soDT, loaiKH, chuThich);
+        }
+
+
+
+        public bool Xoa(int maKH)
+        {
+
+            return DAOKhachHang.Instance.Xoa(maKH);
+        }
+
+        public int TimTheoTen(string tenNV, DataGridView dgvNhanVien)
+        {
+            if (tenNV.Equals(""))
+            {
+                lstKhachHang = DAOKhachHang.Instance.GetKhachHang();
+                dgvNhanVien.DataSource = lstKhachHang;
+                return 0;
+            }
+            else
+            {
+                dgvNhanVien.DataSource = DAOKhachHang.Instance.TimTheoTen(tenNV);
+                return 1;
+            }
+
+        }
+        public int TimTheoMa(string maHH, DataGridView dgvHangHoa)
+        {
+            try
+            {
+
+                dgvHangHoa.DataSource = DAOKhachHang.Instance.TimTheoMa(Convert.ToInt32(maHH));
+                return 1;
+            }
+            catch
+            {
+                lstKhachHang = DAOKhachHang.Instance.GetKhachHang();
+                dgvHangHoa.DataSource = lstKhachHang;
+                return 0;
+            }
+        }
+
+        public List<string> ListTimTheoTen()
+        {
+            return DAOKhachHang.Instance.ListTimTheoTen();
+        }
+        public List<string> ListTimTheoMa()
+        {
+            return DAOKhachHang.Instance.ListTimTheoMa();
+        }
+
+        public void DoiAnh(int maNV, Image image)
+        {
+            DAOKhachHang.Instance.DoiAnh(maNV, image);
+        }
+
         #endregion
     }
 }
